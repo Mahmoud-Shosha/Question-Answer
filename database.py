61d6604ef76a4a93fdfacf7a5dfc87ec83c56056
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-02-15 08:37:40 +0200

    Fix error in DB handlers

--- database.py
+++ database.py
@@ -1,12 +1,13 @@
 from flask import g
 import psycopg2
+from psycopg2.extras import DictCursor
 
 
 def connect_db():
     """Rerturn a connection to the DB"""
     db = psycopg2.connect(
         """postgres://rpadtrqneamhrd:b434aabbb7a47f74595f69d90d71cf611ef644424ecfa0deb18cd9b75daf40d3@ec2-52-23-14-156.compute-1.amazonaws.com:5432/dfedbcj590bfo7""",
-        cursor_factory=psycopg2.extras.DictCursor)
+        cursor_factory=DictCursor)
     db.autocommit = True
     return db
 
